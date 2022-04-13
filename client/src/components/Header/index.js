6a0cc04292b9e5d0c7ea207fc3ff6350ba782a7e
Felicity Brammer <felicitybrammer@gmail.com>
2022-04-13 10:13:18 -0700

    header img

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 function Header() {
     return (
-        <h2>Header Component</h2>
+        <header>
+
+            <img src='../images/distracted.png' />
+            <h2>Header Component</h2>
+
+        </header>
     )
+
 };
 
 export default Header;
