df148f7485535cd29a66b6a168d363cf9ddb7264
Felicity Brammer <felicitybrammer@gmail.com>
2022-04-13 11:30:00 -0700

    last header styling, icon not rendering

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import image from '../../images/distracted.png'
 
 function Header() {
     return (
-        <header>
+        <header className='header'>
 
-            <img src='../images/distracted.png' />
-            <h2>Header Component</h2>
+            <img className='header--image' require={image} alt='meme icon' />
+            <h2 className='header--title'>Meme Generator</h2>
+            <h4 className='header--project'>React Practice</h4>
 
         </header>
     )
