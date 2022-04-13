0d63dd575fab65093e8d64716a03e1b7eace11ac
Felicity Brammer <felicitybrammer@gmail.com>
2022-04-13 11:47:39 -0700

    meme form

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Meme from './components/Meme';
 
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div>
       <Header />
+      <Meme />
     </div>
   );
 }
